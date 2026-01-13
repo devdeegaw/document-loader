46e3e5d01589c88ba3cfa8ef32ea9d542883fae5
sagarsm <sagar.mehendale@gmail.com>
2026-01-13 16:17:15 +0000

    add search controller for API

--- src/main/java/com/rag/documentloader/configuration/DocumentLoaderPipeLine.java
+++ src/main/java/com/rag/documentloader/configuration/DocumentLoaderPipeLine.java
@@ -30,92 +30,86 @@ public class DocumentLoaderPipeLine {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DocumentLoaderPipeLine.class);
 
-  @Bean
-  ApplicationRunner go(FunctionCatalog catalog) {
-    Runnable composedFunction = catalog.lookup(null);
-    return args -> {
-      composedFunction.run();
-    };
-  }
+    @Bean
+    ApplicationRunner go(FunctionCatalog catalog) {
+        Runnable composedFunction = catalog.lookup(null);
+        return args -> {
+            composedFunction.run();
+        };
+    }
 
-  @Bean
-  Function<Flux<byte[]>, Flux<Document>> documentReader() {
-    return resourceFlux -> resourceFlux
-        .map(fileBytes -> new TikaDocumentReader(
-            new ByteArrayResource(fileBytes))
-            .get()
-            .getFirst())
-        .subscribeOn(Schedulers.boundedElastic());
-  }
+    @Bean
+    Function<Flux<byte[]>, Flux<Document>> documentReader() {
+        return resourceFlux -> resourceFlux
+                .map(fileBytes -> new TikaDocumentReader(
+                        new ByteArrayResource(fileBytes))
+                        .get()
+                        .getFirst())
+                .subscribeOn(Schedulers.boundedElastic());
+    }
 
-  @Bean
-  Function<Flux<Document>, Flux<List<Document>>> splitter() {
-    var splitter = new TokenTextSplitter();
-    return documentFlux -> documentFlux
-        .map(incoming -> splitter
-            .apply(List.of(incoming)))
-        .subscribeOn(Schedulers.boundedElastic());
-  }
+    @Bean
+    Function<Flux<Document>, Flux<List<Document>>> splitter() {
+        var splitter = new TokenTextSplitter();
+        return documentFlux -> documentFlux
+                .map(incoming -> splitter
+                        .apply(List.of(incoming)))
+                .subscribeOn(Schedulers.boundedElastic());
+    }
 
-  @Value("classpath:/templates/nameOfTheDocument.st")
-  Resource nameOfTheDocumentTemplateResource;
+    @Value("classpath:/templates/nameOfTheDocument.st")
+    Resource nameOfTheDocumentTemplateResource;
 
-  @Bean
-  Function<Flux<List<Document>>, Flux<List<Document>>> titleDeterminer(ChatClient.Builder chatClientBuilder) {
-
+    @Bean
+    Function<Flux<List<Document>>, Flux<List<Document>>> titleDeterminer(ChatClient.Builder chatClientBuilder) {
         return documentListFlux -> documentListFlux
                 .map(documents -> {
                     if (!documents.isEmpty()) {
                         var firstDocument = documents.getFirst();
+                        var chatClient = chatClientBuilder.build();
 
-    return documentListFlux -> documentListFlux
-        .map(documents -> {
-          if (!documents.isEmpty()) {
-            var firstDocument = documents.getFirst();
+                        var title = chatClient.prompt()
+                                .user(userSpec -> userSpec
+                                        .text(nameOfTheDocumentTemplateResource)
+                                        .param("document", firstDocument.getText()))
+                                .call()
+                                .entity(DocumentTitle.class);
 
-            var title = chatClient.prompt()
-                .user(userSpec -> userSpec
-                    .text(nameOfTheDocumentTemplateResource)
-                    .param("document", firstDocument.getText()))
-                .call()
-                .entity(DocumentTitle.class);
+                        if (title == null) {
+                            LOGGER.warn(
+                                    "Unable to determine the name of a document adding to vector store without title.");
+                            documents = documents.stream().toList();
+                        } else {
+                            LOGGER.info("Determined document title to be {}", title.getTitle());
+                            documents = documents.stream().peek(document -> {
+                                document.getMetadata()
+                                        .put("title", title.getNormalizedTitle());
+                            }).toList();
+                        }
+                    }
+                    return documents;
+                })
+                .subscribeOn(Schedulers.boundedElastic());
+    }
 
-            if (title == null) {
-              LOGGER.warn("Unable to determine the name of a document adding to vector store without title.");
-              documents = documents.stream().toList();
-            } else {
-
-              LOGGER.info("Determined document title to be {}", title.getTitle());
-              documents = documents.stream().peek(document -> {
-                document.getMetadata()
-                    .put("title", title.getNormalizedTitle());
-              }).toList();
-            }
-          }
-
-          return documents;
-        });
-  }
-
-  @Bean
-  Consumer<Flux<List<Document>>> vectorStoreConsumer(VectorStore vectorStore) {
-    return documentFlux -> documentFlux
-        .doOnNext(documents -> {
-          if (!documents.isEmpty()) {
-            var docCount = documents.size();
-            LOGGER.info("Writing {} documents to vector store.", docCount);
-            vectorStore.accept(documents);
-            LOGGER.info("{} documents have been written to vector store.", docCount);
-          }
-        })
-        .doOnComplete(() -> {
-          LOGGER.info("✅ Pipeline completed successfully. All documents processed.");
-        })
-        .doOnError(error -> {
-          LOGGER.error("❌ Error in pipeline: ", error);
-
-        })
-        .subscribe();
-  }
+    @Bean
+    Consumer<Flux<List<Document>>> vectorStoreConsumer(VectorStore vectorStore) {
+        return documentFlux -> documentFlux
+                .doOnNext(documents -> {
+                    if (!documents.isEmpty()) {
+                        var docCount = documents.size();
+                        LOGGER.info("Writing {} documents to vector store.", docCount);
+                        vectorStore.accept(documents);
+                        LOGGER.info("{} documents have been written to vector store.", docCount);
+                    }
+                })
+                .doOnComplete(() -> {
+                    LOGGER.info("✅ Pipeline completed successfully. All documents processed.");
+                })
+                .doOnError(error -> {
+                    LOGGER.error("❌ Error in pipeline: ", error);
+                })
+                .subscribe();
+    }
 
 }
